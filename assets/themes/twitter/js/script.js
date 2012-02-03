8d3776118a7c55b74683a65b4693ec1a5cd465c4
Jack Hsu <jack.hsu@gmail.com>
2012-02-03 15:10:13 +0000

    key shortcuts for search

--- assets/themes/twitter/js/script.js
+++ assets/themes/twitter/js/script.js
@@ -1,2 +1,17 @@
 $(function() {
+    // Clear and blur of search box on Esc press
+    var $q = $('#q').bind('keydown', function(ev) {
+        if (ev.keyCode == 27) {
+            $q.val('').blur();
+            return false;
+        }
+    });
+
+    // Focus of search box on '/' press
+    $(window).bind('keydown', function(ev) {
+        if (ev.keyCode == 191) {
+            $q.focus();
+            return false;
+        }
+    });
 });
